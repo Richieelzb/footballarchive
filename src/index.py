a4f2a234dbbf4b14184894f8b15812cfe5a06492
Lupfumo <lupfumo.badaga@vodacom.co.za>
2026-08-03 01:10:08 +0200

    favicon

--- src/index.py
+++ src/index.py
@@ -6,9 +6,8 @@ homepage_html = f"""
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="icon" type="image/x-icon" sizes="32x32" href="badges/footballarchive.png">
-<link rel="stylesheet"
-href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+<link rel="icon" type="image/png" sizes="32x32" href="badges/footballarchive.png">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 
 <title>Football Statistics Archive</title>
 
